f3886d3d44eeb15748feeb44e984358c0f09988d
Yaye Seydi <yayebalde@gmail.com>
2024-03-04 16:19:11 -0800

    initial commit

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ const shuffle = (list) => {
     - Pass the array to the provided shuffle() function and save the returned (randomized) array into a new variable
     named 'randomUrls'
 
-
     Step 2:
     - Get all image elements (but only the ones inside elements with class 'card') from the DOM using any available DOM methods and store them in a variable named 'images'
 
@@ -56,3 +55,23 @@ const shuffle = (list) => {
     - Append an exclamation point (!) to the .textContent property of the h1 on the page
 
 */
+const urls = [
+    "https://i.ibb.co/sjvVrG3/fruit-plyh.jpg",
+    "https://i.ibb.co/g3tqj4w/fruit-zxcv.jpg",
+    "https://i.ibb.co/88Hy4rD/fruit-wert.jpg",
+    "https://i.ibb.co/NmNMLKF/fruit-astt.jpg",
+    "https://i.ibb.co/qp8Q1G6/fruit-dfgh.jpg",
+    "https://i.ibb.co/9thfVhB/fruit-hjkl.jpg",
+    "https://i.ibb.co/YtTfqvD/fruit-dcnj.jpg",
+    "https://i.ibb.co/CVwKM8Z/fruit-ecuh.jpg",
+    "https://i.ibb.co/ccNyKX0/fruit-uzxc.jpg"
+];
+    const randomUrls = shuffle(urls);
+    
+    const images = document.querySelectorAll('.card img');
+
+images.forEach((image, index) => {
+    image.src = randomUrls[index];
+});
+
+document.querySelector('h1').textContent += '!';
